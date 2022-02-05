b859ee0e55e8bfbb70a51f6e2fcab946bc12ef94
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-02-05 14:45:36 +0330

    places get and post

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const HttpError = require("./models/http-error");
+
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  return next(new HttpError("could not find this page", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "unknown error" });
+});
 
 app.listen(5000);
---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,78 @@
+const HttpError = require("../models/http-error");
+
+const { v4: uuidv4 } = require("uuid");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return next(new HttpError("could not find any places for place id", 404));
+  }
+
+  res.json(place);
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const error = new Error("could not find any places for user id");
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json(place);
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
